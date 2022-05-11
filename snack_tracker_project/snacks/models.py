9895a25c5acb506a789e1b0f20e575304cec94ea
GhaidaMomani <ghaidaalmomani3@gmail.com>
2022-05-11 16:48:43 +0200

    init

---
+++ snack_tracker_project/snacks/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+class Snack(models.Model):
+    purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    name=models.CharField(max_length=64, null= True, blank=True)
+    description = models.TextField()
+    
